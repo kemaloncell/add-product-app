7eb54ec15d5e5b93491d897558dfbbd83ac8ceb5
kemaloncell <57701397+kemaloncell@users.noreply.github.com>
2021-10-25 21:25:30 +0300

    product add functions complete

---
+++ src/components/Products.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="row product-container">
+    <app-porduct v-for="(product, i) in ProductList" :key="i">
+      <img class="card-img-top" :src="product.selectedImage" :alt="ProductList.title" />
+      <div class="card-body">
+        <h5 class="card-title">{{ product.title }}</h5>
+        <small> <strong>Adet : </strong> {{ product.count }} </small>
+        <br />
+        <small> <strong>Fiyat : </strong> {{ product.price }} </small>
+        <br />
+        <small> <strong>Tutar : </strong> {{ product.totalPrice }} </small>
+      </div>
+    </app-porduct>
+  </div>
+</template>
+
+<script>
+import Porduct from './Product.vue';
+import { eventBus } from '../main';
+export default {
+  components: {
+    appPorduct: Porduct,
+  },
+  data() {
+    return {
+      ProductList: [],
+    };
+  },
+  created() {
+    // emitlediğimiz datayı $on ile dinliyoruz neyi diniliyoruz ilk parametreyi
+    eventBus.$on('productAdded', (product) => {
+      this.ProductList.push(product);
+    });
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
+export const eventBus = new Vue({
+  methods: {
+    productAdded(value) {},
+  },
+});
 new Vue({
   el: '#app',
-  render: h => h(App)
-})
+  render: (h) => h(App),
+});
